0b91fac0ceae6628f9a30312e8df696788e7dd4e
Ibragim Darslanov <darslanov2020@mail.ru>
2022-10-17 00:48:52 +0300

    Создал первый метод

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] arrayOne = new string[9] { "SBS", "SAS", "Special", "Boat", "Service", "Strength", "1941", "1940", "22" };
 string[] arrayTwo = new string[arrayOne.Length];
+
+void Enumeration(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if(arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
